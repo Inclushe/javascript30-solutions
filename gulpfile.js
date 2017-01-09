d449e89553ec7420f6f8a4b69dc6ee491e0aeae3
Ethan <inclushe@gmail.com>
2017-01-09 21:05:06 +0000

    Nuclear crisis diverted. You are now registered to use Club Penguin.

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp')
+var sass = require('gulp-sass')
+var bs = require('browser-sync')
+var files = {
+  html: './src/**/*.html',
+  js: './src/**/*.js',
+  sass: './src/**/*.sass',
+  serv: './src'
+}
+
+gulp.task('default', ['sass'], function () {
+  bs.init({
+    server: files.serv
+  })
+  gulp.watch(files.sass, ['sass'])
+  gulp.watch([files.html, files.js], bs.reload)
+})
+
+gulp.task('sass', function () {
+  return gulp.src(files.sass)
+  .pipe(sass({
+    outputStyle: 'expanded'
+  }))
+  .on('error', function (e) {
+    console.error(e.message)
+    this.emit('end')
+  })
+  .pipe(gulp.dest('./src/'))
+  .pipe(bs.stream())
+})
